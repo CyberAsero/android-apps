ba7df00dd0260088f189b9607d5040af8009032c
Marlon <mmacatuno@pushnami.com>
2019-07-30 06:32:44 +0800

    Github push

---
+++ app/src/main/java/com/enfrausa/recyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package com.enfrausa.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Event> mData;
+
+    public RecyclerViewAdapter(Context mContext, List<Event> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.cardview_item_book,parent,false);
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
